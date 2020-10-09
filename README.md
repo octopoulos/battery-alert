318d19ab4e5ebaa07b15c1f9e9c67bb8cefc390b
octopoulos <polluxyz@gmail.com>
2020-10-09 10:27:03 +0200

    support for custom commands

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 Battery Alert
 =============
 
-Problem
+Summary
 -------
-I wrote this script for myself, then realized it could be useful to other people as well.
+Utility to let you know when a battery level is too low or too high on your laptop,
+in order to save its battery lifespan.
 
+I wrote this script for myself and found it very useful on my Surface Pro 7.
+A new battery is not so expensive, but it's tricky to replace it on that laptop, and that's also extra waste/pollution.
+
+Obviously, companies like Microsoft don't want us to easily replace the battery,
+nor do they want us to extend the battery lifespan, because we would buy fewer devices.
+Look at `planned obsolescence`: https://en.wikipedia.org/wiki/Planned_obsolescence
+
+
+The problem
+-----------
 Batteries in laptops can have a short lifespan, especially:
 - if the battery level reaches 0% or 100%
 - if they're charged too quickly (fast charging)
@@ -20,19 +31,48 @@ Some battery saving is achieved if the charge-discharge amplitude is minimized, 
 So, until now, I've been trying to constantly watch the battery status, and of course, most of the time I fail
 to notice that it's too low or too high!
 
-Solution
---------
-Unfortunately, it's not possible to stop the battery from charging the laptop, at least not for all laptops.
-So, this program detects if the battery is too low/high, then emits a random sound.
+Solutions
+---------
+There are 3 ways to deal with this, that I found:
+1) a sound alert, the advantage is that you're still in control, for example in some cases you may not want to stop
+charging the laptop, if you're doing something important like playing some game. The problem is that if you're not there
+to hear the sound, then the laptop will go all the way to 100% which will hurt the battery lifespan.
+
+2) enable/disable charging. Not all manufacturers let you do this without some hacking, so it might not be safe,
+but if you have a computer that gives you the tool to do this, then this is ideal of course, but then maybe they already
+give the option to limit the battery, in which case my script is useless :)
+
+3) enable/disable USB charging, for laptops being charged via USB. I haven't found a way to do this though,
+as the laptop can still be charged even when turned off, but maybe the Operating System can control this sometimes.
+
+The script has some kind of support for all 3 points.
+
+
+Sound alert
+-----------
+So, the first solution is to detects if the battery is too low/high, and then emits a random alert sound.
 The sounds are located in the `sound` folder, and can be replaced with anything, as long as they're `.ogg` or `.wav`.
 
 Because the sound is played every 60 seconds (by default), I'm not forgetting anymore.
 
-Sounds taken from:
-https://freesound.org/people/plasterbrain/sounds/242856/
-https://freesound.org/people/tim.kahn/sounds/83339/
-https://freesound.org/people/VABsounds/sounds/394697/
-https://freesound.org/people/Wagna/sounds/325987/
+
+Custom command
+--------------
+If you have another program or script that can stop your laptop from charging, either by deactiving an USB port
+or by directly stopping the battery, you can specify this with:
+- --run-low : this is where you want to start charging
+- --run-high : this is where you want to stop charging
+
+
+USB powered laptop
+------------------
+Some laptops can be powered via USB, but normally a disabled USB port will still provide power delivery,
+so this solution might not work at all and is experimental.
+The program supports devcon.exe via `--devcon`, this will enable/disable the USB port when the battery level exceeds
+the threshold. This is for Windows only.
+
+https://docs.microsoft.com/en-us/windows-hardware/drivers/devtest/devcon
+
 
 Install
 -------
@@ -43,11 +83,27 @@ pip install -r requirements.txt
 This installs `psutil` and `pygame`.
 
 
-Run
----
-
+Examples
+--------
+1)
 ```
 python battery-alert.py --every 60 --low 25 --high 75
 ```
 
 This checks the battery level every 60 seconds, and raises an alarm if it goes below 25% or above 75%.
+
+2)
+```
+python .\battery-alert.py --high 60 --run-high 'git status'
+```
+
+This runs `git status` when the battery goes above 60%, useless but you get the point!
+You could have it launch firefox for example.
+
+
+Sounds taken from
+-----------------
+https://freesound.org/people/plasterbrain/sounds/242856/
+https://freesound.org/people/tim.kahn/sounds/83339/
+https://freesound.org/people/VABsounds/sounds/394697/
+https://freesound.org/people/Wagna/sounds/325987/
--- battery-alert.py
+++ battery-alert.py
@@ -9,46 +9,81 @@ Battery alert
 from argparse import ArgumentParser
 import os
 from random import random, seed
+from subprocess import run
 from time import sleep, time
 
 import psutil
 from pygame import mixer
 
 
-CUR_FOLDER = os.path.dirname(__file__)
-SOUND_FOLDER = os.path.join(CUR_FOLDER, 'sound')
+LOW = 1
+HIGH = 2
+SOUND_FOLDER = os.path.join(os.path.dirname(__file__), 'sound')
+
+
+def default_int(value: int or str, default: int=None) -> int or None:
+    """Convert a value to an int, on exception, return a default value
+    """
+    if isinstance(value, int):
+        return value
+    if value is None:
+        return default
+
+    try:
+        value = int(float(value))
+    except (TypeError, ValueError):
+        value = default
+    return value
 
 
 class BatteryAlert:
-    def __init__(self, every: int, low: int, high: int):
-        if every < 1:
-            every = 1
-        if high > 100:
-            high = 100
-        if low < 1:
-            low = 1
+    def __init__(self, **kwargs):
+        self.devcon = kwargs.get('devcon')
+        self.every = max(default_int(kwargs.get('every'), 60), 1)
+        self.high = min(default_int(kwargs.get('high'), 75), 100)
+        self.low = max(default_int(kwargs.get('low'), 30), 1)
+        self.no_sound = kwargs.get('no_sound')
+        self.run_high = kwargs.get('run_high')
+        self.run_low = kwargs.get('run_low')
 
-        print(f'Battery alert: every={every} : low={low} : high={high}')
-        self.every = every
-        self.high = high
-        self.low = low
+        print(f'Battery alert: devon={self.devcon} every={self.every} low={self.low} high={self.high}')
         self.sounds = []
         self.sources = []
 
         self.scan_sounds()
         self.go()
 
-    def alert(self, text: str, percent: int):
+    def alert(self, which: str, percent: int):
         """Sound alert
         """
-        print(f"Battery too {text} at {percent}%, {'dis' if text == 'high' else ''}connect the cable!")
-        index = int(random() * len(self.sounds))
-        sound = self.sounds[index]
-        if not sound:
-            sound = mixer.Sound(os.path.join(SOUND_FOLDER, self.sources[index]))
-            self.sounds[index] = sound
+        text = 'high' if which == HIGH else 'low'
+        print(f"Battery too {text} at {percent}%, {'dis' if which == HIGH else ''}connect the cable!")
 
-        sound.play()
+        # 1) play a sound
+        if not self.no_sound:
+            index = int(random() * len(self.sounds))
+            sound = self.sounds[index]
+            if not sound:
+                sound = mixer.Sound(os.path.join(SOUND_FOLDER, self.sources[index]))
+                self.sounds[index] = sound
+            sound.play()
+
+        # 2) run custom commands
+        params = None
+        if which == HIGH:
+            if self.run_high:
+                params = self.run_high
+            elif self.devcon:
+                params = ['devcon', 'disable', self.devcon]
+        elif which == LOW:
+            if self.run_low:
+                params = self.run_low
+            elif self.devcon:
+                params = ['devcon', 'enable', self.devcon]
+
+        if params:
+            result = run(params, capture_output=True, text=True)
+            print(result.stdout)
 
     def go(self):
         """Infinite loop
@@ -62,31 +97,61 @@ class BatteryAlert:
 
             if battery.power_plugged:
                 if percent > self.high:
-                    self.alert('high', percent)
+                    self.alert(HIGH, percent)
             elif percent < self.low:
-                self.alert('low', percent)
+                self.alert(LOW, percent)
 
             sleep(self.every)
 
     def scan_sounds(self):
         """Scan the sound folder
         """
+        if self.no_sound:
+            return
         sources = os.listdir(SOUND_FOLDER)
         self.sources = [source for source in sources if source.endswith(('.ogg', '.wav'))]
         self.sounds = [None] * len(self.sources)
-        print(f"{len(self.sources)} sounds scanned: {', '.join(self.sources)}")
+        text = '\n- '.join([''] + self.sources)
+        print(f"{len(self.sources)} sounds scanned: {text}")
 
 
 def main():
-    parser = ArgumentParser(description='Renter', prog='python __main__.py')
-    add = parser.add_argument
+    parser = ArgumentParser(description='Battery Alert', prog='python battery-alert.py')
 
+    group = parser.add_argument_group('main')
+    add = group.add_argument
     add('--every', nargs='?', default=60, type=int, help='check the battery every x seconds, default=60')
+    add('--high', nargs='?', default=75, type=int, help='set the high threshold level, default=75')
     add('--low', nargs='?', default=30, type=int, help='set the low threshold level, default=30')
-    add('--high', nargs='?', default=80, type=int, help='set the high threshold level, default=75')
-    args = parser.parse_args()
+    add('--no-sound', action='store_true', help='disable the sound, ex: to have custom commands do something better')
+    add('--run-high', nargs='?', default=None, help='custom command to run when the battery level is too high')
+    add('--run-low', nargs='?', default=None, help='custom command to run when the battery level is too low')
 
-    battery_alert = BatteryAlert(args.every, args.low, args.high)
+    group = parser.add_argument_group('devcon')
+    add = group.add_argument
+    add('--devcon', nargs='?', default=None, help='device id that devcon.exe will start/stop')
+    add('--devcon-list', action='store_true', help='list usb hwids using devcon')
+    add('--devcon-start', nargs='?', help='start a device with devcon.exe')
+    add('--devcon-stop', nargs='?', help='stop a device with devcon.exe')
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+    args_set = set(item for item, value in args_dict.items() if value)
+
+    # devcon testing
+    if args_set & {'devcon_list', 'devcon_start', 'devcon_stop'}:
+        if args.devcon_list:
+            params = ['devcon', 'hwids', '=usb']
+        elif args.devcon_start:
+            params = ['devcon', 'enable', args.devcon_start]
+        elif args.devcon_stop:
+            params = ['devcon', 'disable', args.devcon_stop]
+
+        result = run(params, capture_output=True, text=True)
+        print(result.stdout)
+    # run the program
+    else:
+        battery_alert = BatteryAlert(**args_dict)
 
 
 if __name__ == '__main__':
